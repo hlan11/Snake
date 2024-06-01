bd0bee92b5d02ee4bcee5e2eb515f53eeadcd536
hlan11 <155436481+hlan11@users.noreply.github.com>
2024-06-01 19:59:17 +0700

    + Add Simple Snake

---
+++ Assets/Scripts/Snake Move.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMove : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform segmentPrefarb;
+    private List<Transform> _segment;
+    private void Start()
+    {
+        _segment = new List<Transform>();
+        _segment.Add(this.transform);
+    }
+    void Grow()
+    {
+        Transform segment=Instantiate(this.segmentPrefarb);
+        segment.position = _segment[_segment.Count-1].position;
+        _segment.Add(segment);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Food"))
+        {
+            Grow();
+        }
+    }
+    void CheckMove() 
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.velocity=Vector2.up*speed;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb.velocity = Vector2.down * speed;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.velocity = Vector2.left * speed;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.velocity = Vector2.right * speed;
+        }
+    }
+    private void FixedUpdate()
+    {
+        for(int i=_segment.Count-1;i>0;i--)
+        {
+            _segment[i].position = _segment[i - 1].position;
+        }
+    }
+    private void Update()
+    {
+        CheckMove();
+    }
+}
